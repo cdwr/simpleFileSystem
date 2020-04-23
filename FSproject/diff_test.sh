b621743f47ee29af3a9f0ce0a8639ebafb4d0f43
Mykhailo <mikebykhovtsev@gmail.com>
2020-04-22 19:05:22 -0700

    Added test script

---
+++ FSproject/diff_test.sh
@@ -0,0 +1,7 @@
+sudo mount diskimage /mnt
+sudo ls -l /mnt
+sudo diff /mnt/tiny /mnt/tiny
+sudo diff /mnt/small /mnt/small1
+sudo diff /mnt/large /mnt/large1
+sudo diff /mnt/huge /mnt/huge1
+sudo umount /mnt
